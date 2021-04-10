6748bcc760cf825d8dd0120d76f41be00aa8743d
Kesi-P <54447709+Kesi-P@users.noreply.github.com>
2021-04-10 15:04:27 +0200

    MVC-routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { getPosts, creatPosts } from '../controllers/posts.js'
 
 const router = express.Router();
 //start from localhost/posts
-router.get('/', (req, res) => {
-    res.send('This works')
-})
+router.get('/',getPosts);
+router.post('/',creatPosts);
 
 export default router;
