8be962adafcf6906d01c2c8882d4251acbf8a400
Kesi-P <54447709+Kesi-P@users.noreply.github.com>
2021-04-11 14:32:52 +0200

    diapatch,thunk

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 //rfce
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+
+import{ getPosts } from './actions/posts'
 import momories from './images/persian.jpg'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
-
 import useStyles from './styles'
 
 const App = () => {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
     return (
         <Container maxWidth='lg'>
             <AppBar className={classes.appBar} position='static' color='inherit'>
