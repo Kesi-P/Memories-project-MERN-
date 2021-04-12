28ec43e016384792b4e5aaa0ee12f74f3489eb54
Kesi-P <54447709+Kesi-P@users.noreply.github.com>
2021-04-12 15:53:08 +0200

    Display post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,45 @@
 //rfce
 import React from 'react'
 import useStyles from './styles'
-    
-const Post = () => {
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DleteIcon from '@material-ui/icons/Delete'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core'
+import moment from 'moment'
+
+const Post = ({post}) => {
     const classes = useStyles(); 
     return (
         <div>
-            <h1>Post</h1>
+            <Card className={classes.card}>
+                <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+                <div className={classes.overlay}>
+                    <Typography variant='h6'>{post.creator}</Typography>
+                    <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+                </div>
+                <div className={classes.overlay2}>
+                    <Button style={{color:'white'}} size='small' onClick={() => {}}>
+                        <MoreHorizIcon fontSize='default' />
+                    </Button>
+                </div>
+                <div className={classes.details}>
+                    <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
+                </div>
+                <CardContent>
+                    <Typography variant='h5' className={classes.title} gutterBottom>{post.message}</Typography>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                    <Button size='small' color='primary' onClick={() => {}}>
+                        <ThumbUpAltIcon fontSize='small' />
+                        Like
+                        {post.likeCount}
+                    </Button>
+                    <Button size='small' color='primary' onClick={() => {}}>
+                        <DleteIcon fontSize='small' />
+                        Delete
+                    </Button>
+                </CardActions>
+            </Card>
         </div>
     )
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { CircularProgress, Grid } from '@material-ui/core'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
@@ -8,13 +9,18 @@ const Posts = () => {
     //same name as from reducer
     const posts = useSelector(state => state.posts)
     const classes = useStyles(); 
-    console.log(posts)
+    //console.log(posts)
     return (
-        <div>
-           <h1>Post</h1> 
-           <Post/>
-           <Post/>
-        </div>
+        !posts.length ? <CircularProgress /> :
+        (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map(( post ) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
